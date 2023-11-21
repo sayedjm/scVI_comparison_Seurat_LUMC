d041535f8edde81cf6e5acebb37ec04a02475563
sayedjm <jaimymohammadi@gmail.com>
2023-11-21 19:24:32 +0100

    umap added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='workflow',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='sayedjm',
+    author_email='',
+    description=''
+)
